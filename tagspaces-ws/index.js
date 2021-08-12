9e75e05de73b4b1283ba1b70fc786dcbad3b0b04
sytolk <s.marinov@structmedia.com>
2021-08-12 17:55:49 +0300

    ws 1.0.8

--- tagspaces-ws/index.js
+++ tagspaces-ws/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const argv = require("yargs/yargs")(process.argv.slice(2))
+/* const argv = require("yargs/yargs")(process.argv.slice(2))
   .usage("Usage: $0 -port [number]")
   .option("port", {
     alias: "p",
@@ -9,7 +9,9 @@ const argv = require("yargs/yargs")(process.argv.slice(2))
     description: "port",
   })
   .help("h")
-  .alias("h", "help").argv;
+  .alias("h", "help").argv; */
 
+const argv = process.argv.slice(2);
 const httpServer = require("./ws");
-httpServer.createWS(argv.port);
+const port = argv[0] === "-p" ? parseInt(argv[1]) : 8888;
+httpServer.createWS(port); //argv.port);
