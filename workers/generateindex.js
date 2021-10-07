2878dc012fc4acab7b157c300bc840f4657aeed5
sytolk <s.marinov@structmedia.com>
2021-10-07 12:37:59 +0300

    fix indexer command line script

--- workers/generateindex.js
+++ workers/generateindex.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const tsIndexer = require("./tsnodeindexer");
+const { persistIndex, createIndex } = require("tagspaces-platforms/indexer");
 
 const args = process.argv.slice(2);
 if (args.length === 0) {
   console.error("please set args [dirPaths..] to generate index");
 } else {
   for (const dir of args) {
-    tsIndexer.indexer(dir).then((directoryIndex) => {
-      tsIndexer.persistIndex(dir, directoryIndex).then((success) => {
+    createIndex(dir).then((directoryIndex) => {
+      persistIndex(dir, directoryIndex).then((success) => {
         if (success) {
           console.log("Index generated in folder: " + dir);
         }
