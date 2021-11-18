b50f2a48ae6e3bf07f7787564303587785ff7172
Stanimir Marinov <s.marinov@structmedia.com>
2021-11-18 12:44:44 +0200

    fix sharp on linux

--- thumbgen-image/tsimagethumbgen.js
+++ thumbgen-image/tsimagethumbgen.js
@@ -2,8 +2,8 @@
 
 // dependencies
 const async = require("async");
-//const sharp = require("./sharp/index").sharp;
-const sharp = require("sharp");
+const sharp = require("./sharp/index").sharp;
+// const sharp = require("sharp");
 const stream = require("stream");
 
 const tmbMaxWidth = 400;
