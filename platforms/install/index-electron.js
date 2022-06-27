0cece507df4438bfdcc519bcb5335ddb0aa35a1d
sytolk <s.marinov@structmedia.com>
2022-06-27 16:16:07 +0300

    TSN805 - Concept for unifying two configs - electron fixes

--- platforms/install/index-electron.js
+++ platforms/install/index-electron.js
@@ -1,3 +1,4 @@
+/*
 import {
     getLocationPath,
     listMetaDirectoryPromise,
@@ -75,4 +76,13 @@ export {
     isWorkerAvailable,
     createDirectoryIndexInWorker,
     createThumbnailsInWorker
+};
+*/
+
+const ioNode = require("@tagspaces/tagspaces-common-node/io-node");
+const ioElectron = require("@tagspaces/tagspaces-common-electron/io-electron");
+
+module.exports = {
+    ...ioNode,
+    ...ioElectron,
 };
