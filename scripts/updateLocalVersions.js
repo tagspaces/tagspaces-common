d2d167da072092e7b90f70eb0aadc89343020d4c
sytolk <s.marinov@structmedia.com>
2022-07-20 12:13:32 +0300

    update version

---
+++ scripts/updateLocalVersions.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const { getPackages } = require("@lerna/project");
+
+const packagesDir = path.resolve(__dirname, "..", "packages");
+const dynamicDependencies = ["node", "web", "aws", "cordova", "electron"];
+getPackages(packagesDir).then(async (packages) => {
+  if (packages.length === 0) {
+    console.log("No packages found");
+    return;
+  }
+  const packageVersions = packages.reduce(
+    (obj, pkg) => ({ ...obj, [pkg.name]: pkg.version }),
+    {}
+  );
+
+  for (const pkg of packages) {
+    let packageChanged = false;
+
+    for (const dep of dynamicDependencies) {
+      const depGroup = pkg.get(dep + "Dependencies");
+      if (depGroup) {
+        Object.keys(depGroup).forEach(function (key) {
+          const localVersion = packageVersions[key];
+          if (localVersion && depGroup[key] !== localVersion) {
+            depGroup[key] = localVersion;
+            packageChanged = true;
+          }
+        });
+      }
+    }
+    if (packageChanged) {
+      console.log(`Package changed: ${pkg.name}`);
+      // Write changes to disk
+      await pkg.serialize();
+    } /*else {
+      console.log("No package changed");
+    }*/
+  }
+});
