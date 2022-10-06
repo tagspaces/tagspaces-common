c5ee3039c018ff32091a773276c029e552825eca
Stanimir <s.marinov@structmedia.com>
2022-10-06 11:54:22 +0300

    tagspaces-md-update-codemirror-and-add-material-design-icons

--- packages/tagspaces-md/src/CodeMirror.tsx
+++ packages/tagspaces-md/src/CodeMirror.tsx
@@ -1,10 +1,67 @@
 import React from 'react';
-import { basicSetup, EditorState } from '@codemirror/basic-setup';
-import { EditorView, ViewUpdate } from "@codemirror/view"
-import { markdown } from '@codemirror/lang-markdown';
 
+import {
+  autocompletion,
+  closeBrackets,
+  closeBracketsKeymap,
+  completionKeymap
+} from '@codemirror/autocomplete';
+import { defaultKeymap, history, historyKeymap } from '@codemirror/commands';
+import { markdown } from '@codemirror/lang-markdown';
+import {
+  bracketMatching,
+  defaultHighlightStyle,
+  foldGutter,
+  foldKeymap,
+  indentOnInput,
+  syntaxHighlighting
+} from '@codemirror/language';
+import { lintKeymap } from '@codemirror/lint';
+import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
+import { EditorState, Extension } from '@codemirror/state';
+import {
+  crosshairCursor,
+  drawSelection,
+  dropCursor,
+  EditorView,
+  highlightActiveLine,
+  highlightActiveLineGutter,
+  highlightSpecialChars,
+  keymap,
+  lineNumbers,
+  rectangularSelection
+} from '@codemirror/view';
 
 import className from './style.module.css';
+
+const basicSetup: Extension = [
+  lineNumbers(),
+  highlightActiveLineGutter(),
+  highlightSpecialChars(),
+  history(),
+  foldGutter(),
+  drawSelection(),
+  dropCursor(),
+  EditorState.allowMultipleSelections.of(true),
+  indentOnInput(),
+  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+  bracketMatching(),
+  closeBrackets(),
+  autocompletion(),
+  rectangularSelection(),
+  crosshairCursor(),
+  highlightActiveLine(),
+  highlightSelectionMatches(),
+  keymap.of([
+    ...closeBracketsKeymap,
+    ...defaultKeymap,
+    ...searchKeymap,
+    ...historyKeymap,
+    ...foldKeymap,
+    ...completionKeymap,
+    ...lintKeymap
+  ])
+];
 
 type StateOptions = {
   onChange: (getString: () => string) => void;
@@ -26,7 +83,7 @@ const createCodeMirrorState = ({
     extensions: [
       basicSetup,
       markdown(),
-      EditorView.updateListener.of((v: ViewUpdate) => {
+      EditorView.updateListener.of(v => {
         if (v.focusChanged) {
           lock.current = v.view.hasFocus;
         }
