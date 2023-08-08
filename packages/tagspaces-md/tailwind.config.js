6f1e0ed31f1473ef97bfeb65bb345bc1767ab1d7
Stanimir <s.marinov@structmedia.com>
2023-08-08 18:38:06 +0300

    tailwind add blocklist

--- packages/tagspaces-md/tailwind.config.js
+++ packages/tagspaces-md/tailwind.config.js
@@ -3,17 +3,11 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,css}'],
   darkMode: 'class',
   theme: {
-    extend: {
-      /*typography: {
-        DEFAULT: {
-          maxWidth: '100%'
-        }
-      }*/
-    }
+    extend: {}
   },
   future: {
     hoverOnlyWhenSupported: true
   },
   plugins: [require('@tailwindcss/typography'), require('tailwind-nord')],
-  purge: ['.prose']
+  blocklist: ['prose']
 };
