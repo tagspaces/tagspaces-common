60e079551baf9149ee52f86aed1970d09a459138
Stanimir <s.marinov@structmedia.com>
2023-09-11 10:43:06 +0300

    add index.js

---
+++ packages/common/index.js
@@ -0,0 +1,11 @@
+const AppConfig = require("./AppConfig");
+const misc = require("./misc");
+const paths = require("./paths");
+const utilsIo = require("./utils-io");
+
+module.exports = {
+  ...AppConfig,
+  ...misc,
+  ...paths,
+  ...utilsIo,
+};
