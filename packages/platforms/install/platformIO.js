5585dd6e2ae96d6f13bdd4feca834334097d551f
sytolk <s.marinov@structmedia.com>
2023-12-01 10:06:19 +0200

    set default platform = electron

--- packages/platforms/install/platformIO.js
+++ packages/platforms/install/platformIO.js
@@ -2,70 +2,29 @@
 
 const fs = require("fs-extra");
 const path = require("path");
-// const spawn = require("child_process").spawn;
-// const pkg = require("../package.json");
+// const { installDependencies } = require("../../dynamic-packages-loading");
+// const { installDependencies } = require("@tagspaces/dynamic-packages-loading");
 
-// let platform; // = os.platform();
+let platform = process.env.PD_PLATFORM || "electron";
 
-if (process.env.PD_PLATFORM) {
-  const platform = process.env.PD_PLATFORM;
-  console.log("platform specified:", platform);
+if (platform) {
   fs.copySync(
     path.join(__dirname, "index-" + platform + ".js"),
     path.join(__dirname, "..", "index.js")
   );
-} else {
-  console.log("No platform specified");
+} /*else {
   fs.removeSync(path.join(__dirname, "..", "index.js"));
-}
-
-/*const dependencies = platform + "Dependencies";
-const dependenciesObj = pkg[dependencies];
-
-function install(packages, onFinish) {
-  const args = ["install"];
-  args.push("--no-save");
-  args.push("--no-package-lock");
-  args.push(...packages);
-  const proc = spawn("npm", args);
-  proc.on("close", (status) => {
-    if (status === 0) {
-      console.log("success npm install " + packages);
-      onFinish();
-    } else {
-      onFinish(
-        new Error("'npm " + args + "' failed with status " + status)
-      );
-    }
-  });
-}
-
-if (dependenciesObj && Object.keys(dependenciesObj).length) {
-  console.log("Installing dependencies for " + platform);
-  const packages = [];
-
-  for (const dep in dependenciesObj) {
-    // eslint-disable-next-line no-prototype-builtins
-    if (dependenciesObj.hasOwnProperty(dep)) {
-      if (!fs.existsSync(path.join(__dirname, "..", "node_modules", dep))) {
-        packages.push(dep.concat("@").concat(dependenciesObj[dep]));
-      }
-    }
-  }
-
-  if (packages.length > 0) {
-    fs.removeSync(path.join(__dirname, "..", "node_modules"));
-    install(packages, function (er) {
-      if (er) {
-        console.log("Install error:", er);
-      }
-    });
-  } else {
-    console.log(
-      "Installing dependencies for " + platform + " are already installed."
-    );
-  }
-} else {
-  console.log("No specific dependencies on this platform: " + platform);
-  fs.removeSync(path.join(__dirname, "..", "node_modules"));
 }*/
+
+/*async function processInstall() {
+  try {
+    const cwd = "./";
+    // const json = require(pkg);
+    const success = await installDependencies(cwd, platform);
+    //const sum = (await processValue(argv.x)) + (await processValue(argv.y));
+    console.info(`success = ${success}`);
+  } catch (err) {
+    console.info(`${err.message}\n ${await parser.getHelp()}`);
+  }
+}
+processInstall().then(() => console.info("finish"));*/
