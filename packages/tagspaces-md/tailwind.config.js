5bc662b0fbfbaf6250515892ea881054d55032f8
sytolk <s.marinov@structmedia.com>
2024-05-16 13:25:19 +0300

    style fixes

--- packages/tagspaces-md/tailwind.config.js
+++ packages/tagspaces-md/tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
           }
         }
       }
+      /*fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui', 'sans-serif','Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol','Noto Color Emoji']
+      },*/
     }
   },
   future: {
