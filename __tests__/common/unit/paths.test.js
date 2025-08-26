3e1aa230e2f44a3fe9ea51ad1f9609a27b1ead8e
sytolk <s.marinov@structmedia.com>
2025-08-26 17:59:24 +0300

    formatting

--- __tests__/common/unit/paths.test.js
+++ __tests__/common/unit/paths.test.js
@@ -248,7 +248,6 @@ describe("Common Paths unit tests", () => {
   });
 
   test("paths joinPaths", async () => {
-
     const path = paths.joinPaths(
       AppConfig.dirSeparator,
       pathLib.join(__dirname, "..", ".."),
@@ -256,18 +255,11 @@ describe("Common Paths unit tests", () => {
     );
     expect(path).toBe(pathLib.join(__dirname, "..", "..", "img.jpg"));
 
-    const rootPath = AppConfig.isWin ? "\\\\DESKTOP-07OE903\\Users\\smari\\OneDrive\\Картини\\test\\" : "/Users/sytolk/Downloads/Yarndings_20/"
-    const path1 = paths.joinPaths(
-      AppConfig.dirSeparator,
-      rootPath,
-      "new_dir"
-    );
-    expect(path1).toBe(
-      pathLib.join(
-        rootPath,
-        "new_dir"
-      )
-    );
+    const rootPath = AppConfig.isWin
+      ? "\\\\DESKTOP-07OE903\\Users\\smari\\OneDrive\\Картини\\test\\"
+      : "/Users/sytolk/Downloads/Yarndings_20/";
+    const path1 = paths.joinPaths(AppConfig.dirSeparator, rootPath, "new_dir");
+    expect(path1).toBe(pathLib.join(rootPath, "new_dir"));
   });
 
   test("paths generateSharingLink", async () => {
